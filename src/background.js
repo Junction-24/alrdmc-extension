540d94e05031c7429b9b8ea134aeb3636b9a846d
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2024-11-08 22:35:14 +0000

    Mean pooling working

--- src/background.js
+++ src/background.js
@@ -1,4 +1,4 @@
-import { pipeline, env } from '@xenova/transformers';
+import { pipeline, env, mean } from '@xenova/transformers';
 
 // Skip initial check for local models, since we are not loading any local models.
 env.allowLocalModels = false;
@@ -22,20 +22,23 @@ class PipelineSingleton {
 }
 
 const embed = async (text) => {
-  // Get the pipeline instance. This will load and build the model when run for the first time.
-  let model = await PipelineSingleton.getInstance((data) => {
-      // You can track the progress of the pipeline creation here.
-      // e.g., you can send `data` back to the UI to indicate a progress bar
-      // console.log('progress', data)
-  });
-
-  // Actually run the model on the input text
-  let result = await model(text);
-  return result;
+    // Actually run the model on the input text
+    let result = await model(text);
+    return result;
 };
 
+console.log("Loading pipeline...");
+// Get the pipeline instance. This will load and build the model when run for the first time.
+if (PipelineSingleton.instance === null) {
+    let model = await PipelineSingleton.getInstance((data) => {
+        // You can track the progress of the pipeline creation here.
+        // e.g., you can send `data` back to the UI to indicate a progress bar
+        console.log('progress', data)
+    });
+}
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action !== "get_topic_embedding") return;
+    if (message.action !== "get_topic_embedding") return;
 
     console.log("Message received:", message);
 
@@ -43,7 +46,17 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         let embedding = await embed(message.topic);
         console.log("Embedding:", embedding);
 
-        sendResponse(embedding);
+        // Size of the embeddings: [batch_size, num_tokens, embedding_size]
+        // Mean pooling the embeddings across the tokens to get a single embedding for the topic
+        let mean_pooled_embedding = embedding.sum(1).mul(1.0 / embedding.dims[1]);
+
+        if (mean_pooled_embedding.dims[0] > 1) {
+            console.warn("More than one embedding returned. Using the first one.");
+        }
+
+        mean_pooled_embedding = mean_pooled_embedding.view(mean_pooled_embedding.dims[1]);
+
+        sendResponse(mean_pooled_embedding);
     })();
 
     return true;
